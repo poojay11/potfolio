483a39e2dd1cedb40cf377ea7ee75d5a4c4929a6
poojay11 <py2288752@gmail.com>
2021-06-30 12:21:23 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,60 @@
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:3000,
+    reset:true
+});
+
+sr.reveal('.home_title',{})
+sr.reveal('.home_heading',{delay:200})
+sr.reveal('.home_pera',{delay:200})
+
+
+sr.reveal('.about-img',{})
+sr.reveal('.about_heading',{delay:200})
+sr.reveal('.about_pera',{delay:200})
+
+
+
+sr.reveal('."skills_heading',{delay:300})
+sr.reveal('.nome',{delay:300})
+sr.reveal('.percentage-num',{delay:300})
+
+
+sr.reveal('.skill-sr',{delay:400})
+sr.reveal('.pro-bar',{delay:400})
+
+
+sr.reveal('.social-links',{delay:400})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
